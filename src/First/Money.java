d0d1bea6fd77abc7aedbec8688a8c3e1b9ff16a4
1003393 <gusdn@sk.com>
2016-05-10 14:25:02 +0900

    16/05/10

--- src/First/Money.java
+++ src/First/Money.java
@@ -1,11 +1,37 @@
 package First;
 
 public class Money {
-	protected static int amount;
-	
-	public boolean equals(Object object) {
-		Money money = (Money) object;
-		return amount == money.amount;
+	  protected int amount;
+	  protected String currency;
+
+	  Money(int amount, String currency) {
+		  this.amount = amount;
+		  this.currency = currency;
+	  }
+	  
+	  static Dollar dollar(int amount) {
+	    return new Dollar(amount, "USD");
+	  }
+
+	  static Franc franc(int amount) {
+	    return new Franc(amount, "CHF");
+	  }
+
+	  public boolean equals(Object object) {
+	    Money money = (Money) object;
+	    return this.amount == money.amount
+	            && currency().equals(money.currency());
+	  }
+
+	  public Money times(int multiplier) {
+		  return new Money(amount * multiplier, currency);
+	  }
+	  
+	  String currency() {
+		  return currency;
+	  }
+	  
+	  public String toString() {
+		  return amount + " " + currency;
+	  }
 	}
-	
-}
